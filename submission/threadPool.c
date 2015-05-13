0e0956eefc3aa9035e6df63e2e5efa1acb37387d
dori medini <dorimedini@gmail.com>
2015-05-13 21:23:03 +0300

    סידור תיקיות. את הקבצים מפה עורכים

---
+++ submission/threadPool.c
@@ -0,0 +1,14 @@
+
+#include "threadPool.h"
+
+ThreadPool* tpCreate(int numOfThreads) {
+
+}
+
+void tpDestroy(ThreadPool* threadPool, int shouldWaitForTasks) {
+
+}
+
+int tpInsertTask(ThreadPool* threadPool, void (*computeFunc) (void *), void* param) {
+
+}
