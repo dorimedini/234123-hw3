2d15d996703b2d8d4bc20871e12b456357d93fe3
dori medini <dorimedini@gmail.com>
2015-05-23 17:00:27 +0300

    קודם כל, יש הזזת קבצים. דבר שני, עדכנתי את הטסט שלנו שיהיה יותר קול (תודה לרועי על הטיפ איך להדפיס התקדמות...)

--- scripts/update_files.sh
+++ scripts/update_files.sh
@@ -5,7 +5,7 @@ cp -vr /mnt/hgfs/HW/234123-hw3/submission/threadPool.* /root/hw3/
 cp -vr /mnt/hgfs/HW/234123-hw3/submission/Makefile /root/hw3/
 cp -vr /mnt/hgfs/HW/234123-hw3/course_files/test.c /root/hw3/
 cp -vr /mnt/hgfs/HW/234123-hw3/course_files/osqueue.* /root/hw3/
-cp -vr /mnt/hgfs/HW/234123-hw3/tests/our_tester.c /root/hw3/
+cp -vr /mnt/hgfs/HW/234123-hw3/tests/* /root/hw3/
 
 # Unixify
 dos2unix /root/hw3/*
