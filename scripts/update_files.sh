01ebd972c4ef5f2164643e2117c2e74bec45d9d6
dori medini <dorimedini@gmail.com>
2015-05-25 17:26:57 +0300

    גרסה 2 של התרגיל
    הפעם בלי נעילת CREW לשדה state.
    זה יוצא הרבה יותר פשוט ויש מצב שיורידו לנו נקודות בגלל מה שהמתרגל אמר
    בפיאצה... זה מסורבל ומיותר.
    העתקתי את הגרסה החדשה (כולל תיעוד מתאים) ךתיקייה נפרדת, והזזתי את התיעוד
    הקודם לתיקייה המתאימה לגרסה שלו

--- scripts/update_files.sh
+++ scripts/update_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy
-cp -vr /mnt/hgfs/HW/234123-hw3/submission/threadPool.* /root/hw3/
+cp -vr /mnt/hgfs/HW/234123-hw3/submission_NO_CREW/threadPool.* /root/hw3/
 cp -vr /mnt/hgfs/HW/234123-hw3/submission/Makefile /root/hw3/
 cp -vr /mnt/hgfs/HW/234123-hw3/course_files/test.c /root/hw3/
 cp -vr /mnt/hgfs/HW/234123-hw3/course_files/osqueue.* /root/hw3/
